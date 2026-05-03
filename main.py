ffff9c9ed8fc3ba7c916ab4a7c40ff6bed9dbfb9
pvivian3 <pvivian3@nd.edu>
2026-05-02 20:03:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,27 @@ def main():
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--learning_rate", type=float, default=1e-3)
     parser.add_argument("--epochs", type=int, default=5)
+
+    if model == "mnist":
+        class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    elif model ==  "cifar10":
+        class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    else: # cifar100
+        class_names = [
+    'apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 
+    'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel', 
+    'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock', 
+    'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur', 
+    'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster', 
+    'house', 'kangaroo', 'keyboard', 'lamp', 'laptop', 'leopard', 'lion', 
+    'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse', 
+    'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear', 
+    'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine', 
+    'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose', 
+    'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake', 
+    'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table', 
+    'tank', 'telephone', 'television', 'tiger', 'tractor', 'train', 'trout', 
+    'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf', 'woman', 'worm' ]
  
     args = parser.parse_args()
     with open(args.noisecfg, "r") as f:
@@ -72,7 +93,8 @@ def main():
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-    
+
+    cnn(device, test_loader, model, test_dataset, train_dataset, class_names)
     
     print("Finished training, now running with TMR...")
     results = run_with_tmr(model, test_loader, device, config)
