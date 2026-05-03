d9951e5a25b3940aa95cb0cb91a14a9aae2d39c3
pvivian3 <pvivian3@nd.edu>
2026-05-02 20:29:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -76,13 +76,13 @@ def main():
     model = model_map[args.model]().to(device)
 
     train_loader, test_loader = data.get_dataloaders(args.task, train_batch_size=args.batch_size, test_batch_size=args.batch_size)
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay = 0.005, momentum = 0.9)
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, weight_decay = 0.005, momentum = 0.9)
    
     criterion = nn.CrossEntropyLoss(ignore_index=-1)
 
     if cfg["noise_training"]:
         # add noise but not one time noise
-        model = clone_with_noisy_layers(model, one_time_noise_sd=0.0, layer_noise_sd=config.noise_sd1, add_one_time_noise=config.add_one_time_noise, add_quantization=config.add_quantization, quantize_fn=config.quantize_fn, include_name_contains=config.include_name_contains, exclude_name_contains=config.exclude_name_contains)
+        model = clone_with_noisy_layers(model, one_time_noise_sd=0.0, layer_noise_sd=config.noise_sd, add_one_time_noise=config.add_one_time_noise, add_quantization=config.add_quantization, quantize_fn=config.quantize_fn, include_name_contains=config.include_name_contains, exclude_name_contains=config.exclude_name_contains)
     
     model.train()
     print("Starting training...")
